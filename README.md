75400cb15467b1db6d9b274589656cbd50477a3b
Danny Eiden <deiden@myvr.com>
2016-09-21 18:37:47 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodebb-plugin-reputation-for-posting
+Give a user reputation every time they post
+
+## Installation
+`npm install nodebb-plugin-reputation-for-posting`
+
+## NodeBB Version
+This widget has been tested on NodeBB 1.1.2
+
+## Features
+* When a user posts a topic or a comment, the user will be awarded reputation
+* Control how much reputation a user will be awarded in the admin settings
---
+++ library.js
@@ -0,0 +1,61 @@
+(function(module) {
+    "use strict";
+
+    var async = module.parent.require('async'),
+        db = module.parent.require('./database'),
+        meta = module.parent.require('./meta'),
+        user = module.parent.require('./user'),
+        rewards = module.parent.require('./rewards');
+
+    var plugin = {};
+
+    plugin.init = function(params, callback) {
+        var app = params.router,
+            middleware = params.middleware;
+
+        app.get('/admin/plugins/reputation-for-posting', middleware.admin.buildHeader, renderAdmin);
+        app.get('/api/admin/plugins/reputation-for-posting', renderAdmin);
+
+        callback();
+    };
+
+    function renderAdmin(req, res, next) {
+        res.render('admin', {});
+    }
+
+    plugin.awardReputation = function(postData) {
+        async.waterfall([
+            function(next){
+                meta.settings.get('reputation-for-posting', next);
+            },
+            function(settings, next) {
+                user.incrementUserFieldBy(
+                    postData.uid,
+                    'reputation',
+                    settings.reputationAwardAmount,
+                    next
+                );
+            },
+            function(newreputation, next) {
+                db.sortedSetAdd('users:reputation', newreputation, postData.uid, next);
+            },
+            function(next) {
+                rewards.checkConditionAndRewardUser(postData.uid, 'essentials/user.reputation', function(callback) {
+                    user.getUserField(postData.uid, 'reputation', callback);
+                });
+            }
+        ]);
+    };
+
+    plugin.addAdminNavigation = function(header, callback) {
+        header.plugins.push({
+            route: '/plugins/reputation-for-posting',
+            icon: 'fa-trophy',
+            name: 'Reputation for Posting'
+        });
+
+        callback(null, header);
+    };
+
+    module.exports = plugin;
+}(module));
